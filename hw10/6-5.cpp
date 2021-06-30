fa039a265f94374e16e71ffab6297163900cd4a0
freezeCPU <77972345+freezeCPU@users.noreply.github.com>
2021-06-30 17:43:14 +0800

    Update 6-5.cpp

--- hw10/6-5.cpp
+++ hw10/6-5.cpp
@@ -1,30 +1,6 @@
-#include <iostream>
-#include <cstdio>
-#include <string>
-#include <iomanip>
-#include <algorithm>
-#include <cmath>
-
-using namespace std;
-
-template<class T>
-class Number{
-    T a;
-    public:
-    Number(T a):a(a){}
-   friend ostream & operator<<(iostream & o, const Number & N);
-    const T & Max( const Number & N)
-    {
-        return a>N.a?a:N.a;
-    }
-    const T & Min(const Number & N)
-    {
-        
-    }
-};
-
-int main()
-{
-   
-   return 0;
+template<typename T>
+const T & Max(const T &a, const T &b){
+    if(a>b)
+        return a;
+    return b;
 }
